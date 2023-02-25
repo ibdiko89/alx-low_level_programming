b94b54761f2ea3ff17caa9c218e2c3231f4247f5
ibdiko89 <ibdiko89@gmail.com>
2023-02-24 19:01:48 -0800

    working with betty to correct my code

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,48 +1,28 @@
-#include "main.h"
+#include"main.h"
 
 /**
- *  * print_number - prints an integer
- *   * @n: random integer
- *    * Return: void
- *     */
+ *  * print_number - Prints an integer.
+ *   *
+ *    * @n: The integer to be printed.
+ *
+     */
+
 void print_number(int n)
 {
-		char ld, cr;
-			int r;
-				int ct = 0;
+	unsigned int num = n;
 
-					if (n < 0)
-							{
-										_putchar ('-');
-												ld = (char)('0' - (n % 10));
-														n = n / -10;
-															}
+	/*first check if its negative*/
+		if (n < 0)
+	{
+			_putchar('-');
+			num = -num;
+		}
 
-						else
-								{
-											ld = (char)((n % 10) + '0');
-													n = n / 10;
-														}
+	/*print the first few digits*/
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-							r = 0;
-								while (n > 0)
-										{
-													r = r * 10 + (n % 10);
-															n = n / 10;
-																	ct++;
-																		}
+	/*print the last digit*/
+	_putchar((num % 10) + 48);
 
-									while (r > 0)
-											{
-														cr = (char)((r % 10) + '0');
-																_putchar(cr);
-																		r = r / 10;
-																				ct--;
-																					}
-										while (ct != 0)
-												{
-															_putchar('0');
-																	ct--;
-																		}
-											_putchar(ld);
 }
