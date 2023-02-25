1c122ddb65d982168be9a427b9cad52c17d635b1
ibdiko89 <ibdiko89@gmail.com>
2023-02-24 18:13:19 -0800

    revisited

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
- *  * main - print largest prime factor of 612852475143
- *   * Return: 0 on Success
+ *  * main - Prints the largest prime factor of the number 612852475143
+ *   * Return: Always 0 (Success)
  *    */
-int main(void)
-{
-		unsigned long num = 612852475143;
-			unsigned long i;
-				unsigned long large_prime;
 
-					for (i = 2; i <= num; i++)
-							{
-										if (num % i == 0)
-													{
-																	large_prime = i;
-																				num /= large_prime;
-																						}
-											}
-						printf("%lu\n", large_prime);
-							return (0);
-}
+int main(void)
+	
+{
+
+        long x, maxf;
+        long number = 612852475143;
+        double square = sqrt(number);
+
+        for (x = 1; x <= square; x++)
+        {
+	        if (number % x == 0)
+	        {
+		        maxf = number / x;
+		        }
+	        }
+        printf("%ld\n", maxf);
+        return (0);
+
+}}
