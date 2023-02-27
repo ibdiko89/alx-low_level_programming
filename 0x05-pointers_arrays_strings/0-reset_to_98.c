266a93c7ccf71289b6e6be467605126d1afc0b80
ibdiko89 <ibdiko89@gmail.com>
2023-02-27 07:06:05 -0800

    working with pointer and arrays. created readme  and the first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98 and resets it
+ * @n: pointer to the variable to update
+ * it run through the  4 stages of compiling. uses betty -style
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
