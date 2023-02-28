37d5d21b04b2a21c764617791b8c932df4d70cce
ibdiko89 <ibdiko89@gmail.com>
2023-02-27 18:07:45 -0800

    revised version

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int - swaps the values of two integers
- * @a: pointer to the first value
- * @b: pointer to second value 
+ * @a: pointer to first value
+ * @b: pointer to second value
  */
 void swap_int(int *a, int *b)
 {
