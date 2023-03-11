c1e67a90c25f1c4a6fc14e42a52268f3ed4bc74c
ibdiko89 <ibdiko89@gmail.com>
2023-03-10 18:29:19 -0800

    insert your commit message

---
+++ 0x09-static_libraries/.gitignore/_putchar.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+3. MAIN.H
+
+vi main.h
+
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char c);
+
+#endif /* MAIN_H_INCLUDED */
